df312ee6f7387a8fa10bed193922264ab4e7b103
Marianna Żurek <zurekmarianna@gmail.com>
2022-10-25 23:50:11 +0200

    mobile navigation & footer update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const navigation = document.querySelector(".navigation");
 const hamburgerOpen = document.querySelector(".hamburger_open");
 const hamburgerClose = document.querySelector(".hamburger_close");
+const basket = document.querySelector(".shop");
+const openBasket = document.querySelector(".basket_container");
+const closeBasket = document.querySelector(".shop_close");
 
 hamburgerOpen.addEventListener("click", () => {
    navigation.classList.add("visible");
@@ -8,4 +11,12 @@ hamburgerOpen.addEventListener("click", () => {
 
 hamburgerClose.addEventListener("click", () => {
     navigation.classList.remove("visible");
-})
+});
+
+openBasket.addEventListener("click", () => {
+    basket.classList.add("visible");
+});
+
+closeBasket.addEventListener("click", () => {
+    basket.classList.remove("visible");
+});
