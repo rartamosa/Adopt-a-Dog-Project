ce8597a162a39bd9fe40ad8bac2f6a0e43c7e712
Marianna Żurek <zurekmarianna@gmail.com>
2022-10-31 16:04:02 +0100

    header desktop update

--- app.js
+++ app.js
@@ -22,13 +22,3 @@ closeBasket.addEventListener("click", () => {
   basket.classList.remove("visible");
 });
 
-window.onscroll = function() {myFunction()};
-var navbar = document.querySelector(".header");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
